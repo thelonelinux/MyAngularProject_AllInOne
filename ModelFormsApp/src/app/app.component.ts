37fe34ee7ea689e2ec3310d6e53271d91f1b4db8
thelonelinux <vickythethechguy@gmial.com>
2021-01-15 14:31:53 +0530

    Reactive Form in angular 10

---
+++ ModelFormsApp/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import{FormGroup,FormControl,FormBuilder,Validators,NgForm} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ModelFormsApp';
+
+  signupForm:FormGroup;
+  FirstName:string="";
+  LastName:string="";
+  Email:string="";
+  Password:string="";
+
+  constructor(private frmbuilder:FormBuilder){
+    this.signupForm=frmbuilder.group({
+      fname:['',Validators.required],
+      lname:['',Validators.required],
+      Emailid:['',[Validators.required, Validators.email]],
+      userpassword:['',Validators.required]
+    });
+  }
+
+  ngOnInit(){}
+
+  PostData(signupForm:any){
+    this.FirstName=signupForm.controls.fname.value;
+    console.log(signupForm.controls);
+  
+  }
+}
---
+++ ModelFormsApp/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import{ReactiveFormsModule} from '@angular/forms'
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,ReactiveFormsModule, NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
